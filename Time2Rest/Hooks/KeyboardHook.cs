251f1bb2d6f197fc029d02b698b51841d9be0b31
SDchao <wentaochens@outlook.com>
2021-09-01 16:17:38 +0800

    Add Keyboard Hook

--- Time2Rest/Hooks/KeyboardHook.cs
+++ Time2Rest/Hooks/KeyboardHook.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
+using NLog;
 namespace Time2Rest.Hooks
 {
     class KeyboardHook
@@ -25,12 +26,14 @@ namespace Time2Rest.Hooks
         const int WH_KEYBOARD_LL = 13;
 
         static int hKeyboardHook = 0;
+        HookProc KeyboardHookProcDelegate;
+        private readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
 
         private int KeyboardHookProc(int nCode, Int32 wParam, IntPtr lParam)
         {
             if (nCode >= 0)
             {
-                Console.WriteLine("Keyboard Captured!!!");
+                Logger.Debug("Key: {0} {1}", wParam, lParam);
             }
 
             return CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
@@ -39,27 +42,36 @@ namespace Time2Rest.Hooks
 
         public void StartHook()
         {
+            Logger.Debug("Starting Hook");
             if (hKeyboardHook == 0)
             {
-                HookProc keyboardHookProc = new HookProc(KeyboardHookProc);
-                hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, keyboardHookProc, Marshal.GetHINSTANCE(Assembly.GetExecutingAssembly().GetModules()[0]), 0);
+                KeyboardHookProcDelegate = new HookProc(KeyboardHookProc);
+                hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProcDelegate, Marshal.GetHINSTANCE(Assembly.GetExecutingAssembly().GetModules()[0]), 0);
+
+                Logger.Info("Hooked Keyboard");
 
                 if (hKeyboardHook == 0)
                 {
                     StopHook();
-                    throw new Exception("Hook failed!");
+                    Logger.Error("Unable to start hook");
+                    throw new Exception("Hook failed");
                 }
             }
         }
 
         public void StopHook()
         {
+            Logger.Debug("Stoping Hook");
             if (hKeyboardHook != 0)
             {
                 bool retKeyboard = UnhookWindowsHookEx(hKeyboardHook);
                 hKeyboardHook = 0;
+                Logger.Info("Unhooked keyboard");
                 if (!retKeyboard)
-                    throw new Exception("Unhook failed!");
+                {
+                    Logger.Error("Unable to unhook");
+                    throw new Exception("Unhook failed");
+                }
             }
         }
     }
