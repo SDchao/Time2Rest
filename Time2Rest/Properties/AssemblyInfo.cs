a7c71538a54e4e2be685b1e90c495db50a0d9d54
SDchao <wentaochens@outlook.com>
2021-09-03 13:36:40 +0800

    Add Assembly Info

--- Time2Rest/Properties/AssemblyInfo.cs
+++ Time2Rest/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Time2Rest")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Have a break for your eyes!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SDchao")]
 [assembly: AssemblyProduct("Time2Rest")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
