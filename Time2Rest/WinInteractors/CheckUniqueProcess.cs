cd2013cdf13d4c76d24a9e241c584d74ae2dd652
SDchao <wentaochens@outlook.com>
2021-09-17 11:27:06 +0800

    Fix slow startup

--- Time2Rest/WinInteractors/CheckUniqueProcess.cs
+++ Time2Rest/WinInteractors/CheckUniqueProcess.cs
@@ -6,14 +6,15 @@ namespace Time2Rest.WinInteractors
     {
         public static bool CheckUnique()
         {
-            string name = Process.GetCurrentProcess().MainModule.FileName;
+            //string name = Process.GetCurrentProcess().MainModule.FileName;
+            string name = Process.GetCurrentProcess().ProcessName;
             int id = Process.GetCurrentProcess().Id;
             Process[] prc = Process.GetProcesses();
             foreach (Process pr in prc)
             {
                 try
                 {
-                    if ((name == pr.MainModule.FileName) && (pr.Id != id))
+                    if ((name == pr.ProcessName) && (pr.Id != id))
                     {
                         return true;
                     }
